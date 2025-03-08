e75d4341ab6743b140be94ed8e5e97d56501c118
Frenz86 <frenzy86@gmail.com>
2025-03-08 16:05:32 +0100

    new

--- password.py
+++ password.py
@@ -2,6 +2,43 @@ LOGIN = {
 'test@gmail.com': '1234',
 'riccardo.silvi@unibo.it': 'futura',
 'danielegrottiuk@gmail.com':'futura'
-
+'gio.medugno@gmail.com': 'futura',
+'Fabrizio.muzzi@gmail.com': 'futura',
+'cristian.andreola@gmail.com': 'futura',
+'roberto.bortoletto@gmail.com': 'futura',
+'mdalco@gmail.com': 'futura',
+'francesco.savini@fastercouplings.com': 'futura',
+'antonino.mastroeni@bbscommunity.it': 'futura',
+'mauro.rambaldini@fastercouplings.com': 'futura',
+'marco.laghi89@gmail.com': 'futura',
+'renzo.lazzarin@gmail.com': 'futura',
+'andreiachiurato@gmail.com': 'futura',
+'bernardi.ndr@gmail.com': 'futura',
+'marcoalfano87@gmail.com': 'futura',
+'castelli.c.77@gmail.com': 'futura',
+'lorenzo.tramuto@safety21.it': 'futura',
+'elisa.magellano@gmail.com': 'futura',
+'alessandro.giorgi@bbscommunity.it': 'futura',
+'alberto.suprani@bbscommunity.it': 'futura',
+'fabio.galletti@bbscommunity.it': 'futura',
+'tommaso.calvi@fastercouplings.com': 'futura',
+'luca.tadioli@fastercouplings.com': 'futura',
+'solferini@hotmail.it': 'futura',
+'manera.sergio@gmail.com': 'futura',
+'luciocarlo72@gmail.com': 'futura',
+'e.copertaro@gmail.com': 'futura',
+'elipizzy@gmail.com': 'futura',
+'antonino.mastroeni93@gmail.com': 'futura',
+'mail.marcobello@gmail.com': 'futura',
+'riccardodelsante@gmail.com': 'futura',
+'mauro.rambaldini@bbscommunity.it': 'futura',
+'Sergio.manera@bbscommunity.it': 'futura',
+'elisa.pizzardo@bbscommunity.it': 'futura',
 
         }
+
+
+
+
+
+
